68c68bd9a9a1811f0f3dd86eac76439f29c6915e
parkseryu <pjh971212@gmail.com>
2023-11-05 01:05:31 +0900

    rename prqueue

--- Algorithm Notes/Data Structure/선형/데크, 우선 순위 큐/Priority_queue1.py
+++ Algorithm Notes/Data Structure/선형/데크, 우선 순위 큐/Priority_queue1.py
@@ -1,25 +1,12 @@
-import heapq
+from queue import PriorityQueue
+q = PriorityQueue()
+q.put(2)
+q.put(1)
+q.put(3)
 
-# 빈 우선순위 큐 생성
-my_priority_queue = []
+x = q.get()
+y = q.get()
+z = q.get()
+print(x, y, z)
 
-# 원소를 포함한 우선순위 큐 생성
-my_priority_queue_with_elements = [3, 1, 4, 1, 5, 9, 2, 6]
-
-# 우선순위 큐에 원소 추가
-heapq.heappush(my_priority_queue, 5)
-heapq.heappush(my_priority_queue, 2)
-heapq.heappush(my_priority_queue, 8)
-
-# 우선순위 큐에서 최솟값 추출
-min_value = heapq.heappop(my_priority_queue)
-
-# 우선순위 큐에서 최솟값 조회 (제거하지 않고)
-peek_min_value = my_priority_queue[0]
-
-# 원소를 추가하지 않고 최솟값 조회
-min_value_without_push = heapq.nsmallest(1, my_priority_queue_with_elements)[0]
-
-# 우선순위 큐 순회(iteration)
-for element in my_priority_queue_with_elements:
-    print(element)
+# 1, 2, 3
