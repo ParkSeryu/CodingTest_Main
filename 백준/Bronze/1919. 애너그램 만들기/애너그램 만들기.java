58e752d7f51a7166cb60c1b818bc4b36a853ebd8
Park JunHyeong <pjh971212@gmail.com>
2023-12-22 01:53:45 +0900

    [Bronze II] Title: 애너그램 만들기, Time: 204 ms, Memory: 17532 KB -BaekjoonHub

--- 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.java
+++ 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.java
@@ -3,26 +3,25 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String a = sc.next();
-        String b = sc.next();
+        String str1 = sc.next();
+        String str2 = sc.next();
 
-        int[] countA = new int[26];
-        int[] countB = new int[26];
-
-        for(int i = 0; i < a.length(); i++){
-            countA[a.charAt(i) - 'a']++;
-        }
-        for(int i = 0; i < b.length(); i++){
-            countB[b.charAt(i) - 'a']++;
-        }
+        int[] countA = getAlphabetCount(str1);
+        int[] countB = getAlphabetCount(str2);
 
         int ans = 0;
-        for(int i = 0; i < 26; i++){
-            if(countA[i] > countB[i])
-                ans += countA[i] - countB[i];
-            else
-                ans += countB[i] - countA[i];
+        for (int i = 0; i < 26; i++) {
+            ans += Math.abs(countA[i] - countB[i]);
         }
         System.out.println(ans);
+    }
+
+    private static int[] getAlphabetCount(String str) {
+        int[] count = new int[26];
+        for (int i = 0; i < str.length(); i++) {
+            count[str.charAt(i) - 'a']++;
+
+        }
+        return count;
     }
 }
