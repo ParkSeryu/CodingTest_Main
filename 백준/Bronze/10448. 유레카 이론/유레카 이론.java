7b32253a2edd451723209c11c015e6428ffabd4e
Park JunHyeong <pjh971212@gmail.com>
2023-12-26 00:17:09 +0900

    [Bronze I] Title: 유레카 이론, Time: 588 ms, Memory: 22012 KB -BaekjoonHub

--- 백준/Bronze/10448. 유레카 이론/유레카 이론.java
+++ 백준/Bronze/10448. 유레카 이론/유레카 이론.java
@@ -2,38 +2,51 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+    
+//    static boolean[] isEurekaNumber = new boolean[1001];
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        getTriangleNumbers(arrayList, 1000);
+        
+
         int K = sc.nextInt();
         while (K-- > 0) {
-            int answer = 0;
             int su = sc.nextInt();
+            int answer = isEurekaNumber(arrayList, su);
+            System.out.println(answer);
+        }
+    }
 
-            ArrayList<Integer> arrayList = new ArrayList<>();
+    public static void getTriangleNumbers(ArrayList<Integer> arrayList, int max) {
+        int T = 0;
+        int n = 1;
+        while (T < max) {
+            T = n * (n + 1) / 2;
+            arrayList.add(T);
+            n++;
+        }
+    }
 
-            int n = 1;
-            int T = 0;
-            while (T < su) {
-                T = n * (n + 1) / 2;
-                arrayList.add(T);
-                n++;
-            }
-
-            for(int i = 0; i < arrayList.size(); i++){
-                for(int j = 0; j < arrayList.size(); j++){
-                    for(int k = 0; k < arrayList.size(); k++){
-                        if(arrayList.get(i) + arrayList.get(j) + arrayList.get(k) == su){
-                            answer = 1;
-                            break;
-                        }
+    public static int isEurekaNumber(ArrayList<Integer> arrayList, int su) {
+        int answer = 0;
+        for (int i = 0; i < arrayList.size(); i++) {
+            for (int j = 0; j < arrayList.size(); j++) {
+                for (int k = 0; k < arrayList.size(); k++) {
+                    if (arrayList.get(i) + arrayList.get(j) + arrayList.get(k) == su) {
+                        answer = 1;
+                        break;
                     }
                 }
             }
-
-            System.out.println(answer);
-
         }
 
+        return answer;
     }
-
 }
+
+// BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+// BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//            bw.write(a + b + "\n");
+//            bw.flush();
