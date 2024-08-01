083f4429bd5b3ba987c4114abc5eb029162f2c8f
ParkSeryu <pjh971212@gmail.com>
2024-08-01 23:14:16 +0900

    [Bronze III] Title: 웰컴 키트, Time: 236 ms, Memory: 18364 KB -BaekjoonHub

---
+++ 백준/Bronze/30802. 웰컴 키트/웰컴 키트.java
@@ -0,0 +1,29 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        //System.setIn(new FileInputStream("src/main/java/sample/input.txt"));
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] arr = new int[6];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int T = sc.nextInt();
+        int P = sc.nextInt();
+
+        int answer_T = 0;
+        for (int i : arr) {
+            answer_T += (int) Math.ceil((double) i / T);
+        }
+
+        System.out.println(answer_T);
+        System.out.print(N / P + " ");
+        System.out.print(N % P);
+
+    }
+}
