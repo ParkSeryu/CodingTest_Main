0b894e6fc82cb13902e6445923c094d1187d4d3f
ParkSeryu <pjh971212@gmail.com>
2024-08-04 23:19:11 +0900

    [Silver III] Title: 안테나, Time: 1184 ms, Memory: 185464 KB -BaekjoonHub

---
+++ 백준/Silver/18310. 안테나/안테나.java
@@ -0,0 +1,26 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            list.add(sc.nextInt());
+        }
+
+        Collections.sort(list);
+
+        System.out.println(list.get(Math.round((float)((N - 1) / 2))));
+
+
+    }
+}
