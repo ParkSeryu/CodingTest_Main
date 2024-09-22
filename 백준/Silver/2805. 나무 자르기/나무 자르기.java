c243746eb3361e4b4a33904aeeabe0e2507a22e9
ParkSeryu <pjh971212@gmail.com>
2024-09-22 18:43:01 +0900

    [Silver II] Title: 나무 자르기, Time: 1636 ms, Memory: 320216 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -0,0 +1,46 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        long M = sc.nextInt();
+        int[] trees = new int[N];
+
+        for (int i = 0; i < trees.length; i++) {
+            trees[i] = sc.nextInt();
+        }
+
+        System.out.println(parametric_search(M, trees));
+    }
+
+    private static long parametric_search(long height, int[] trees) {
+        long start = 0;
+        long end = Long.MAX_VALUE;
+        long result = 0;
+
+        while (start <= end) {
+            long middle = (start + end) / 2;
+            if (getHeight(middle, trees) < height) {
+                end = middle - 1;
+            } else {
+                start = middle + 1;
+                result = middle;
+            }
+        }
+        return result;
+    }
+
+    private static long getHeight(long height, int[] trees) {
+        long result = 0;
+        for (int tree : trees) {
+            long h = tree - height;
+            if (h > 0) {
+                result += h;
+            }
+        }
+        return result;
+    }
+}
